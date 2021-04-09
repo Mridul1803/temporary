a87d4f6c851456571f52bf1e05e44bc3d7e9e6ea
Mridul Jain <jainmridul2000@gmail.com>
2021-04-09 20:35:13 +0530

    featureCommit

--- a.js
+++ a.js
@@ -1,5 +1,10 @@
 console.log("helloo !!");
 
+//While
+
 //git checkout -b featur1       - creates a new branch feature1
 // git checkout master          - to switch to master branch
 // git branch                   - to see current branch
+
+// git branch -d feature1           - to delete branch locally
+// git push origin --delete feature1    - to delete branch from cloud
--- b.js
+++ b.js
@@ -1,3 +1,3 @@
-console.log("b.js");
+console.log("Feature branch");
 
 // vscode://vscode.github-authentication/did-authenticate?windowid=1&code=b19a506b10bec0c28a59&state=a38ac5ad-401a-41ec-9137-886287fbc510
