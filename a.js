2f265f86da02fa9281f6a773b8458a781701a24c
Mridul Jain <jainmridul2000@gmail.com>
2021-04-09 20:08:13 +0530

    helloCommit

--- a.js
+++ a.js
@@ -1,1 +1,5 @@
-console.log("a.js");
+console.log("helloo !!");
+
+//git checkout -b featur1       - creates a new branch feature1
+// git checkout master          - to switch to master branch
+// git branch                   - to see current branch
--- b.js
+++ b.js
@@ -1,1 +1,3 @@
 console.log("b.js");
+
+// vscode://vscode.github-authentication/did-authenticate?windowid=1&code=b19a506b10bec0c28a59&state=a38ac5ad-401a-41ec-9137-886287fbc510
